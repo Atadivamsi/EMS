07d812a062eeb663eff3a5cac5f5bdce3a13b046
Atadivamsi <atadivamsi@gmail.com>
2022-03-31 14:40:06 +0530

    first commit

---
+++ src/components/UI/Toolbar/Toolbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Toolbar = props => (
+	<nav className="navbar navbar-expand-lg navbar-light bg-light">
+		<Link className="navbar-brand" to="/">Home</Link>		
+		<div className="collapse navbar-collapse" id="toolbar">
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<Link className="nav-link" to="/employees">Employees</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/projects">Projects</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/tasks">Tasks</Link>
+				</li>
+				<li className="nav-item" >
+					<Link className="nav-link" to="/Login">Login</Link>
+				</li>
+				<li className="nav" style={{float:'right'}}>
+					<Link className="nav-link" to="/tasks">Register</Link>
+				</li>
+			</ul>
+			
+		</div>
+	</nav>
+)
+
+export default Toolbar
